ea4318c209f742f258b92e6d17df847d4dd81076
Somit Meshram <somitmeshram3256@gmail.com>
2025-01-13 12:42:58 +0530

    Time: 0 ms (100%), Space: 41.7 MB (5.23%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        int sIndex = 0,tIndex=0;
+        while(sIndex<s.Length && tIndex<t.Length){
+            if(s[sIndex]==t[tIndex])
+                sIndex++;
+            tIndex++;
+        }
+        return sIndex==s.Length;
+    }
+}
