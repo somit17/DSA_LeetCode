7638c2dbde1dfb9b0c801e896e10e63fd074c405
Somit Meshram <somitmeshram3256@gmail.com>
2025-01-15 12:37:33 +0530

    Time: 18 ms (60.12%), Space: 45.4 MB (39.62%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        List<char> result = new List<char>();
+        foreach(var ch in s){
+            if(Char.IsLetterOrDigit(ch))
+                result.Add(Char.ToLower(ch));
+        }
+        int L=0,R=result.Count-1;
+        while(L<R){
+            if(result[L]!=result[R]){
+                return false;
+            }
+            L++;
+            R--;
+        }
+        return true;
+    }
+}
