921f8d5dd44fe55364967f5f7f16075ca3712b4d
Somit Meshram <somitmeshram3256@gmail.com>
2025-03-04 15:17:47 +0530

    Time: 0 ms (100%), Space: 49.6 MB (72.58%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cs
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cs
@@ -1,7 +1,36 @@
 public class Solution {
     public int SingleNonDuplicate(int[] nums) {
         //Approach 1 using Hashmap
-        return GetNonDuplicate(nums);
+        //return GetNonDuplicate(nums);
+
+        //Approach 2 using BS
+        return GetNonDuplicateBS(nums);
+    }
+
+    public int GetNonDuplicateBS(int[] nums){
+        int n = nums.Length;
+        int l=0,r=n-1;
+        while(l<r){
+           int mid = l+(r-l)/2;
+           bool isEven = (r-mid)%2==0;
+            if(nums[mid] == nums[mid+1]) {
+                if(isEven) {
+                    l = mid+2;
+                } else {
+                    r = mid-1;
+                }
+            } else if(nums[mid] == nums[mid-1]) {
+                if(isEven) {
+                    r = mid-2;
+                } else {
+                    l = mid+1;
+                }
+            } else {
+                return nums[mid];
+            }
+        }
+        return nums[r];
+
     }
     public int GetNonDuplicate(int[] nums){
         Dictionary<int,int> dic = new();
