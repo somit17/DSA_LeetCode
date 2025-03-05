0e2ffbcc0d8eb749cde175ba371c0485250988a8
Somit Meshram <somitmeshram3256@gmail.com>
2025-03-05 16:31:07 +0530

    Time: 10 ms (41.97%), Space: 50.5 MB (53.2%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cs
+++ 0207-course-schedule/0207-course-schedule.cs
@@ -1,63 +1,66 @@
 public class Solution {
     public bool CanFinish(int numCourses, int[][] prerequisites) {
-     int count = 0;
-        Dictionary<int, List<int>> adjList = new();
-        int[] indegree = new int[numCourses];
-        System.Array.Fill(indegree, 0);
-
-        // Initialize adjacency list and indegree array
-        for (int i = 0; i < numCourses; i++)
-        {
-            adjList[i] = new List<int>();
+        Dictionary<int,List<int>> adjList = new();
+        for(int i=0;i<numCourses;i++){
+            adjList[i]=new();
+        }
+        int[] indegree= new int[numCourses];
+        //Build the graph
+        foreach(var data in prerequisites){
+            int u=data[1],v=data[0];
+            adjList[u].Add(v);
+            indegree[v]++;
         }
 
-        // Build the graph
-        foreach (var prereq in prerequisites)
-        {
-            int src = prereq[1], dest = prereq[0];
-            adjList[src].Add(dest);
-            indegree[dest]++;
-        }
+        
+        bool[] visited = new bool[numCourses];
+        bool[] inRecursion = new bool[numCourses];
 
-        Queue<int> queue = new Queue<int>();
-        for (int i = 0; i < indegree.Length; i++)
-            if (indegree[i] == 0)
-            {
-                queue.Enqueue(i);
-                count++;
-            }
+        Array.Fill(visited,false);
+        Array.Fill(inRecursion ,false);
 
-        while (queue.Count > 0)
-        {
-            var currentNode = queue.Dequeue();
-            foreach (var v in adjList[currentNode])
-            {
-                indegree[v]--;
-                if (indegree[v] == 0)
-                {
-                    count++;
-                    queue.Enqueue(v);
-                }
+        return BFSCycleDetection(adjList,indegree,numCourses);
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i] && DFSCycleDetection(adjList,visited,inRecursion,i)){
+                return false;
             }
         }
-
-        if (count == numCourses) return true;
-        return false;
+        return true;
     }
 
-    public bool IsCycle(Dictionary<int, List<int>> adjList, bool[] visited, bool[] inRecurrsion, int u)
-    {
-        visited[u] = true;
-        inRecurrsion[u] = true;
-        foreach (var v in adjList[u])
-        {
-            if (!visited[v] && IsCycle(adjList, visited, inRecurrsion, v))
-                return true;
-            else if (inRecurrsion[v])
-                return true;
+    public bool DFSCycleDetection(Dictionary<int,List<int>> adjList,bool[] visited,bool[] inRecursion,int u)    {
+        visited[u]=true;
+        inRecursion[u]=true;
+        foreach(var v in adjList[u]){
+            if(!visited[v]){
+                if(DFSCycleDetection(adjList,visited,inRecursion,v))
+                    return true;
+            }else if(inRecursion[v]){
+                return true; 
+            }
         }
+        inRecursion[u]=false;
+        return false;
+    }
+    public bool BFSCycleDetection(Dictionary<int,List<int>> adjList,int[] indegree,int numCourses){
+        Queue<int> queue = new();
+        for(int i=0;i<indegree.Length;i++){
+            if(indegree[i]==0)
+                queue.Enqueue(i);
+        }
+        int processedNodes = 0;
+        while(queue.Count > 0){
+            int node = queue.Dequeue();
+            processedNodes++;
 
-        inRecurrsion[u] = false;
-        return false;   
+            foreach(var v in adjList[node]){
+                indegree[v]--;
+                if(indegree[v]==0)
+                    queue.Enqueue(v);
+            }
+
+        }
+        // Step 4: Check if all nodes were processed
+        return processedNodes == numCourses;
     }
 }
