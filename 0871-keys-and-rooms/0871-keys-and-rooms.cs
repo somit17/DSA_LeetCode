b1bc95b7ce08d3b08efd29aaee5010b455ffa09f
Somit Meshram <somitmeshram3256@gmail.com>
2025-03-11 12:28:10 +0530

    Time: 4 ms (39.26%), Space: 49.6 MB (5.17%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cs
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cs
@@ -7,7 +7,8 @@ public class Solution {
             adj[i]=new List<int>(rooms[i]);
         }
         bool[] visited = new bool[n];
-        DFS(adj,visited,0);
+        //DFS(adj,visited,0);
+        BFS(adj,visited);
         foreach(var status in visited){
             if(!status)
                 return false;
@@ -20,6 +21,22 @@ public class Solution {
         foreach(var v in adj[u]){
             if(!visited[v])
                 DFS(adj,visited,v);
+        }
+    }
+
+    public void BFS(Dictionary<int,List<int>> adj,bool[] visited){
+        Queue<int> queue = new();
+        queue.Enqueue(0);
+        visited[0]=true;
+
+        while(queue.Count>0){
+            var node = queue.Dequeue();
+            foreach(var v in adj[node]){
+                if(!visited[v]){
+                    queue.Enqueue(v);
+                    visited[v]=true;
+                }
+            }
         }
     }
 }
