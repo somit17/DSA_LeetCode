6a12c91011bb05b072d6e0243db03bf2dceac546
Somit Meshram <somitmeshram3256@gmail.com>
2025-03-12 16:54:51 +0530

    Time: 3 ms (43.28%), Space: 42.9 MB (19.72%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public char FindTheDifference(string s, string t) {
+        Dictionary<char,int> map = new();
+        foreach(var ch in s){
+            if(!map.ContainsKey(ch)){
+                map[ch]=1;
+            }else{
+                map[ch]++;
+            }
+        }
+        foreach(var ch in t){
+            if(!map.ContainsKey(ch)){
+                return ch;
+            }else{
+                map[ch]--;
+                if(map[ch]==-1)
+                    return ch;
+            }
+        }
+        return 'x';
+    }
+}
